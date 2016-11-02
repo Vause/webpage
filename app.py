5a22ab66f039232956812e9bade12bd4afc43c72
vause@yahoo.com <vause@yahoo.com>
2016-11-02 10:10:05 -0500

    adding shitty login page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, session, request
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, Markup
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
+import os
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -27,74 +28,94 @@ def background_thread():
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+    if not session.get('logged_in'):
+	    return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def do_student_login():
+	if request.form['password'] == 'password' and request.form['username'] == 'Jane.Doe':
+		return render_template('student.html', async_mode=socketio.async_mode)
+		session['logged_in'] = True
+		
+	elif request.form['password'] == 'Adminpass1234' and request.form['username'] == 'ruben.arutyunov':
+		return render_template('teacher.html', async_mode=socketio.async_mode)
+		session['logged_in'] = True
+		
+	else:
+		flash(Markup('Successfully registered, please click <a href="/me" class="alert-link">here</a>'))
+	return index()
+		
 @app.route('/student')
 def student_page():
-    return render_template('student.html', async_mode=socketio.async_mode)
-
+	if not session.get('logged_in'):
+		return index()
+	else:
+		return render_template('student.html', async_mode=socketio.async_mode)
 
 @app.route('/teacher')
 def teacher_page():
-    return render_template('teacher.html', async_mode=socketio.async_mode)
+	if not session.get('logged_in'):
+		return index()
+	else:
+		return render_template('teacher.html', async_mode=socketio.async_mode)
 
 
 
 @socketio.on('my_event', namespace='/test')
 def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
+    session['logged_in'] = session.get('logged_in', 0) + 1
     emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+         {'data': message['data'], 'count': session['logged_in']})
 
 
 @socketio.on('my_broadcast_event', namespace='/test')
 def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
+    session['logged_in'] = session.get('logged_in', 0) + 1
     emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
+         {'data': message['data'], 'count': session['logged_in']},
          broadcast=True)
 
 
 @socketio.on('join', namespace='/test')
 def join(message):
     join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
+    session['logged_in'] = session.get('logged_in', 0) + 1
     emit('my_response',
          {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+          'count': session['logged_in']})
 
 
 @socketio.on('leave', namespace='/test')
 def leave(message):
     leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
+    session['logged_in'] = session.get('logged_in', 0) + 1
     emit('my_response',
          {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+          'count': session['logged_in']})
 
 
 @socketio.on('close_room', namespace='/test')
 def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
+    session['logged_in'] = session.get('logged_in', 0) + 1
     emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
+                         'count': session['logged_in']},
          room=message['room'])
     close_room(message['room'])
 
 
 @socketio.on('my_room_event', namespace='/test')
 def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
+    session['logged_in'] = session.get('logged_in', 0) + 1
     emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
+         {'data': message['data'], 'count': session['logged_in']},
          room=message['room'])
 
 
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
+    session['logged_in'] = session.get('logged_in', 0) + 1
     emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
+         {'data': 'Disconnected!', 'count': session['logged_in']})
     disconnect()
 
 
@@ -113,6 +134,11 @@ def test_connect():
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected', request.sid)
+	
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
 
 
 if __name__ == '__main__':
