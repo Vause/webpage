7ad5c38d3ec0f00102f2154a87e3904389ca477c
root <root@Hedorah>
2016-11-02 14:25:33 +0000

    this

---
+++ test_stuff.py
@@ -0,0 +1,10 @@
+def do_thing(thing):
+    return thing
+
+def test_useless():
+    assert do_thing("cake") == "cake"
+    assert do_thing("not_cake") != "cake"
+
+def test_useless2():
+    assert 1 == 1
+
