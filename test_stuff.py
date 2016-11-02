1b7389d1bd5e9493fe5b3696b2885a370ac1cb43
root <root@Hedorah>
2016-11-02 14:35:36 +0000

    fix test_file

--- test_stuff.py
+++ test_stuff.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-=======
-from app import *
-
-
->>>>>>> f524f139dc2d6d954cc3a0277949074b50f63189
-def do_thing(thing):
-    return thing
 
 def test_useless():
     assert do_thing("cake") == "cake"
@@ -13,7 +5,3 @@ def test_useless():
 
 def test_useless2():
     assert 1 == 1
-<<<<<<< HEAD
-
-=======
->>>>>>> f524f139dc2d6d954cc3a0277949074b50f63189
